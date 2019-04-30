6a3f39f6ed33a05808c6a2ed52b1ff18cf3bf9e2
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-30 20:17:06 +0300

    Bind AxiosInstance to Xsolla.

--- src/Xsolla.ts
+++ src/Xsolla.ts
@@ -1,4 +1,5 @@
 import { XsollaMerchantCredentials, XsollaProjectCredentials } from './Xsolla.interface';
+import Axios, { AxiosInstance } from 'axios';
 
 export default class Xsolla {
 
@@ -8,10 +9,21 @@ export default class Xsolla {
     private readonly credentials: XsollaMerchantCredentials;
 
     /**
+     * REST client
+     */
+    protected readonly client: AxiosInstance;
+
+    /**
      * Xsolla constructor
      */
     public constructor(credentials: XsollaMerchantCredentials) {
         this.credentials = credentials;
+        this.client = Axios.create({
+            auth: {
+                username: credentials.merchantId,
+                password: credentials.apiKey,
+            }
+        });
     }
 
 }
