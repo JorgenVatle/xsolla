baf2a6fb8a4fe6508cf1a45ed873eaafc0c847e6
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-30 19:51:42 +0300

    Add XsollaProjectCredentials interface.

--- src/Xsolla.interface.ts
+++ src/Xsolla.interface.ts
@@ -1,4 +1,5 @@
 export interface XsollaMerchantCredentials {
+
     /**
      * Xsolla merchant ID
      */
@@ -8,5 +9,24 @@ export interface XsollaMerchantCredentials {
      * Xsolla Merchant API Key
      */
     apiKey: string;
+
+}
+
+export interface XsollaProjectCredentials {
+
+    /**
+     * Xsolla Merchant ID
+     */
+    merchantId: string;
+
+    /**
+     * Xsolla Project ID
+     */
+    projectId: string;
+
+    /**
+     * Xsolla Project Secret
+     */
+    secret: string;
 
 }
