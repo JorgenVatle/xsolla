32b7fe586066fb927506912eed95ba42698b5012
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-01 15:39:58 +0300

    Extract response data from Project methods.

--- src/Project.ts
+++ src/Project.ts
@@ -1,4 +1,3 @@
-import { AxiosPromise } from 'axios';
 import { Create, Get, Update } from './Project.interface';
 import Xsolla from './Xsolla';
 
@@ -7,25 +6,27 @@ export default class Project extends Xsolla {
     /**
      * Create a project.
      */
-    public create(data: Create.input): AxiosPromise<Create.response> {
+    public create(data: Create.input): Promise<Create.response> {
         return this.client.post(`/merchants/${data.merchant_id || this.credentials.merchantId}/projects`,{
             method: 'POST',
             data,
-        })
+        }).then(({ data }) => data);
     }
 
     /**
      * Get a single project by ID.
      */
-    public get(data: Get.input): AxiosPromise<Get.response> {
-        return this.client.get(`/projects/${data.project_id}`);
+    public get(data: Get.input): Promise<Get.response> {
+        return this.client.get(`/projects/${data.project_id}`)
+            .then(({ data }) => data);
     }
 
 
     /**
      * Update a project.
      */
-    public update(data: Update.input): AxiosPromise<Update.response> {
-        return this.client.put(`/projects/${data.project_id}`, data);
+    public update(data: Update.input): Promise<Update.response> {
+        return this.client.put(`/projects/${data.project_id}`, data)
+            .then(({ data }) => data);
     }
 }
