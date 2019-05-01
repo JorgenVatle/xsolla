74b5e62ca8b3726677997ef45a463ba488808a34
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-01 16:39:55 +0300

    Return new Project instances from ProjectCollection get() and create() methods.

--- src/Collections/ProjectCollection.ts
+++ src/Collections/ProjectCollection.ts
@@ -1,24 +1,30 @@
-import { Create, Get, Update } from '../Interfaces/Project.interface';
+import { Create, Get } from '../Interfaces/Project.interface';
 import Collection from './Collection';
+import Project from '../Models/Project';
 
-export default class ProjectCollection extends Collection {
+export default class ProjectCollection extends Collection<Project> {
+
+    /**
+     * Model this collection is responsible for.
+     */
+    protected model = Project;
 
     /**
      * Create a project.
      */
-    public create(data: Create.input): Promise<Create.response> {
+    public create(data: Create.input): Promise<Project> {
         return this.client.post(`/merchants/${data.merchant_id || this.client.merchantId}/projects`,{
             method: 'POST',
             data,
-        }).then(({ data }) => data);
+        }).then(({ data }) => this.createModel(data));
     }
 
     /**
      * Get a single project by ID.
      */
-    public get(data: Get.input): Promise<Get.response> {
+    public get(data: Get.input): Promise<Project> {
         return this.client.get(`/projects/${data.project_id}`)
-            .then(({ data }) => data);
+            .then(({ data }) => this.createModel(data));
     }
 
 }
