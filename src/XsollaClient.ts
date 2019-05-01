be17fc8922ed781f9e416ced2e5396ac0f1e4459
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-01 16:00:01 +0300

    Add XsollaClient class.

---
+++ src/XsollaClient.ts
@@ -0,0 +1,58 @@
+import { XsollaMerchantCredentials } from './Interfaces/Xsolla.interface';
+import Axios, { AxiosInstance } from 'axios';
+
+export default class XsollaClient {
+
+    /**
+     * Xsolla merchant credentials.
+     */
+    protected readonly credentials: XsollaMerchantCredentials;
+
+    /**
+     * REST client
+     */
+    protected readonly client: AxiosInstance;
+
+    /**
+     * Xsolla Client constructor
+     */
+    public constructor(credentials: XsollaMerchantCredentials) {
+        this.credentials = credentials;
+        this.client = Axios.create({
+            baseURL: `https://api.xsolla.com/merchant/v2/`,
+            auth: {
+                username: credentials.merchantId.toString(),
+                password: credentials.apiKey,
+            }
+        });
+    }
+
+    /**
+     * Client get() method.
+     */
+    public get get() {
+        return this.client.get;
+    }
+
+    /**
+     * Client put() method.
+     */
+    public get put() {
+        return this.client.put;
+    }
+
+    /**
+     * Client post() method.
+     */
+    public get post() {
+        return this.client.post;
+    }
+
+    /**
+     * Client request() method.
+     */
+    public get request() {
+        return this.client.request;
+    }
+
+}
