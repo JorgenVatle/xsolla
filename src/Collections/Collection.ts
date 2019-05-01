ad08f84f78f30c34470d5999175c55bffd29ac34
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-01 16:22:00 +0300

    Add abstract model property to Collection.

--- src/Collections/Collection.ts
+++ src/Collections/Collection.ts
@@ -1,6 +1,12 @@
 import XsollaClient from '../XsollaClient';
+import Model from '../Models/Model';
 
 export default abstract class Collection {
+
+    /**
+     * Model this collection is responsible for.
+     */
+    protected abstract model: Model;
 
     /**
      * Xsolla Client
