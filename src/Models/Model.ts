a0055c77e8eb0f01730ddbdd16de6181fd220b40
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-01 16:07:18 +0300

    Add abstract Model class.

---
+++ src/Models/Model.ts
@@ -0,0 +1,23 @@
+import XsollaClient from '../XsollaClient';
+
+export default abstract class Model {
+
+    /**
+     * Xsolla Client
+     */
+    protected client: XsollaClient;
+
+    /**
+     * Model data.
+     */
+    protected data: any;
+
+    /**
+     * Model constructor.
+     */
+    public constructor(data: any, client: XsollaClient) {
+        this.data = data;
+        this.client = client;
+    }
+
+}
