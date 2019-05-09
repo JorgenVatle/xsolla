c8ef34a38a60cd398915ea9e352986d33ca4735c
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-09 16:50:23 +0300

    Add JSDocs to XsollaExceptionCode.

--- src/Exception/XsollaException.ts
+++ src/Exception/XsollaException.ts
@@ -1,9 +1,14 @@
+/**
+ * @link https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_errors
+ */
 export type XsollaExceptionCode = 'INVALID_USER' | 'INVALID_PARAMETER' | 'INVALID_SIGNATURE' | 'INCORRECT_AMOUNT' | 'INCORRECT_INVOICE';
 
 export default abstract class XsollaException extends Error {
 
     /**
      * Xsolla Error Code
+     *
+     * @link https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_errors
      */
     public abstract readonly name: XsollaExceptionCode;
 
